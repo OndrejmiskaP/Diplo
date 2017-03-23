8d3cb734ec6b8787a417a85bcb71514600702990
OndrejmiskaP <peter.ondrejmiska@gmail.com>
2017-03-23 20:27:55 +0100

    🔇

---
+++ LoadPage_Interface.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include "LoadPage_Interface.h"	
+
+LoadPage_Interface::LoadPage_Interface(sWebPage *page)
+{
+}
+
+LoadPage_Interface::~LoadPage_Interface()
+{
+}
+
+void LoadPage_Interface::set_parameter(char *type, char* value)
+{
+
+}
+
+
+
+
+void main()
+{
+	
+
+}
