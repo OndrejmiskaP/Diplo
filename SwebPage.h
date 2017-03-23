f36a3208b0bd3646f12719e2655bd9a7303f6b63
OndrejmiskaP <peter.ondrejmiska@gmail.com>
2017-03-23 20:25:02 +0100

    Create SwebPage.h

---
+++ SwebPage.h
@@ -0,0 +1,22 @@
+#pragma once
+class SwebPage
+{
+public:
+	SwebPage();
+	~SwebPage();
+
+	struct sWebPage
+	{
+		int size_haeder;
+		int size_array;
+		int size_body;
+		char *header;
+		char *array;
+		char *body;
+	} *page;
+
+
+private:
+
+	
+};
